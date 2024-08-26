28d569692cf9eabf329171e068be48eb89bf054e
derzsky <jesuscreator@hotmail.com>
2024-08-26 15:23:51 +0200

    Initial version has been implemented as Stacked Area Series

---
+++ Front/Models.cs
@@ -0,0 +1,20 @@
+﻿namespace Front
+{
+	public class EvGenerator
+	{
+		private double _chance;
+		private Random _random;
+
+		public EvGenerator(double chance)
+		{
+			_chance = chance;
+			_random = new Random();
+		}
+
+		public bool HaveWon()
+		{
+			var nextDouble = _random.NextDouble();
+			return nextDouble >= _chance / 100;
+		}
+	}
+}
