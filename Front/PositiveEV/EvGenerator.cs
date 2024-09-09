ad62c6867a8a6f3ea57b2d5fe40e21c6694f0ce6
derzsky <jesuscreator@hotmail.com>
2024-09-09 12:00:41 +0200

    Each Sim has its folder

---
+++ Front/PositiveEV/EvGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace Front.PositiveEV
+{
+    public class EvGenerator
+    {
+        private Random _random;
+
+        public EvGenerator()
+        {
+            _random = new Random();
+        }
+
+        public bool HasWon(double chance)
+        {
+            var nextDouble = _random.NextDouble();
+            return nextDouble >= 1 - chance / 100;
+        }
+    }
+}
