b1f293c1403333d5ce8ab69801df1dd49bf5d013
derzsky <jesuscreator@hotmail.com>
2024-10-13 13:25:59 +0200

    Empty controls and model template have been created

---
+++ WpfFront/WarDamage/Model.cs
@@ -0,0 +1,30 @@
+﻿namespace WpfFront.WarDamage
+{
+    public class Government
+    {
+        public double GunPower { get; set; }
+        public double EconomicPower { get; set; }
+        public double ManPower { get; set; }
+
+        public double MaxGunPowerSupply { get; set; }
+        public double MaxManPowerSupply { get; set; }
+
+        public double GunPowerThreshold { get; set; }
+        public double EconomicPowerThreshold { get; set; }
+        public double ManPowerThreshold { get; set; }
+
+        public double ManLoss { get; set; }
+
+        public bool WantsToWar
+        {
+            get
+            {
+                return GunPower >= GunPowerThreshold
+                    && EconomicPower >= EconomicPowerThreshold
+                    && ManPower >= ManPowerThreshold;
+            }
+        }
+        
+
+    }
+}
